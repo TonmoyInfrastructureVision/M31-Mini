a39602bbf72ab7b1a86c97b3dd4b38ddbcac8237
eshanized <m.eshanized@gmail.com>
2025-04-30 04:23:47 +0530

    refactor: update react-query import to @tanstack/react-query for improved compatibility

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { AppProps } from 'next/app';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import '../styles/globals.css';
 
 const queryClient = new QueryClient({
